5c54c0ce8977ee8e0ba6623d506896642d1e2561
freddinmg <freddinmg@gmail.com>
2025-04-19 20:23:10 -0500

    added script

--- nav_script.js
+++ nav_script.js
@@ -1,6 +1,8 @@
-$(".nav-button").on("click", function () {
-    $(".nav-links").toggle("fold");
-})
-$(".nav-close-button").on("click", function () {
-    $(".nav-links").hide("fold");
-})
+$(document).ready(function () {
+    $(".nav-button").on("click", function () {
+        $(".nav-links").toggle("fold");
+    })
+    $(".nav-close-button").on("click", function () {
+        $(".nav-links").hide("fold");
+    })
+});
